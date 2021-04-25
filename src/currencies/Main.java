056bb5b3a35ab3de82f739b0ad9e9cc8ba6fc103
PawelBudniak <pawel.b714@gmail.com>
2021-04-26 01:01:31 +0200

    cleanup main

--- src/currencies/Main.java
+++ src/currencies/Main.java
@@ -14,29 +14,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-//        FileReader fp = null;
-//        try {
-//            fp = new FileReader("data/input", "r");
-//        } catch (FileNotFoundException e) {
-//            System.err.println("This file does not exist");
-//            System.exit(1);
-//        }
-//        Lexer lexer = new Lexer(fp);
-//        Token token = lexer.getNextToken();
-//        while (token.getType() != TokenType.T_EOT){
-//            System.out.println(token);
-//        }
-
         try (RandomAccessFile fp = new RandomAccessFile("data/input", "rw")){
-        //try (InputStream in = new FileInputStream("data/input")){
-            //Reader reader = new InputStreamReader(in, "utf-8");
-            //fp.writeChar('C');
-            //fp.seek(0);
-            //System.out.println(""+ (char)reader.read());
-            //System.out.println((char)fp.readByte());
-            //System.exit(1);
-
-            //RandomAccessFile fp = null;
 
             Lexer lexer = new Lexer(new FileReader(fp));
             Token token = lexer.getNextToken();
